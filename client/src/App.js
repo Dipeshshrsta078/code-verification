7af28977f825824b8a2636822cf7d52891f2c278
dipesh_shrestha <dipesh.shrestha@ebpearls.com>
2024-05-02 16:53:32 +0545

    frontend code validation adde

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import CodeVerification from "./components/CodeVerification";
+function App() {
+  return (
+    <div className="App">
+      <CodeVerification />
+    </div>
+  );
+}
+
+export default App;
